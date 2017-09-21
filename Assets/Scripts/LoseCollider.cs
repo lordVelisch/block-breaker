5e60eb4903acffad527de760a2f9855755458644
Valentin Mayer <valentinmayer1@gmail.com>
2017-09-21 22:09:01 +0200

    Bin noch immer zu faul

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager manager;
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger");
+        manager.LoadLevel("Win");
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+        
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    
+    static MusicPlayer tmp = null; 
+	// Use this for initialization
+	void Start () {
+        if (tmp != null)
+        {
+            Destroy(gameObject);
+        }
+        else {
+            tmp = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
